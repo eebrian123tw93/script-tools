c802265cfbd7bfa1d44316424b34183edb00c0fd
Brian93 <eebrian123tw93@gmail.com>
2024-01-24 00:27:38 +0800

    update

--- obsiden-to-hexo/main.py
+++ obsiden-to-hexo/main.py
@@ -17,5 +17,6 @@ note_names.append('UART RS232 RS484')
 note_names.append('SparkFun Thing Plus Matter - MGM240P')
 note_names.append('IIC I2C （Inter-Integrated Circuit）')
 note_names.append('實作 Nginx 負載平衡 load balance')
+note_names.append('20240123小日常')
 for note_name in note_names:
     Convert(obsiden_path=obsiden_path, hexo_path=hexo_path, note_name=note_name).start()
